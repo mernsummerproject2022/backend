b8793b4908df3bd0f00fc7b02578bd9d877cc642
cristian.cirdei01@e-uvt.ro <83004817+cris-alt@users.noreply.github.com>
2022-08-04 16:40:24 +0300

    remove requests + email redirect

--- src/database/databaseOperations.js
+++ src/database/databaseOperations.js
@@ -10,146 +10,180 @@ const UserModule = require("../models/user");
 const User = UserModule.User;
 
 export function ValidId(param) {
-    return mongoose.Types.ObjectId.isValid(param);
+  return mongoose.Types.ObjectId.isValid(param);
 }
 
 export async function userExists(email) {
-    return await User.findOne({ email });
+  return await User.findOne({ email });
 }
 export async function userIdExists(id) {
-    return await User.findOne({ _id: id });
+  return await User.findOne({ _id: id });
 }
 export async function ownerEvents(ownerId) {
-    const objectId = new ObjectId(ownerId);
-    return await Event.find({ 'owner._id': objectId });
+  const objectId = new ObjectId(ownerId);
+  return await Event.find({ "owner._id": objectId });
 }
 
 export async function allEvents() {
-    return await Event.find({}).select(EVENT_SELECTOR);
+  return await Event.find({}).select(EVENT_SELECTOR);
 }
 
 export async function oneEvent(eventId) {
-    const objectId = new ObjectId(eventId);
-    return await Event.findById(objectId);
+  const objectId = new ObjectId(eventId);
+  return await Event.findById(objectId);
 }
 
 export async function addInvite(eventId, userEmail) {
-    const newInvite = new Invite({accepted: 'undefined', user: {email: userEmail}});    
+  const newInvite = new Invite({
+    accepted: "undefined",
+    user: { email: userEmail }
+  });
 
-    const responseEvent = await Event.find({ _id: new ObjectId(eventId)},
+  const responseEvent = await Event.find(
+    { _id: new ObjectId(eventId) },
     {
       invites: {
         $elemMatch: {
-          "user.email": userEmail,
+          "user.email": userEmail
         }
       }
-  });
+    }
+  );
 
-  if(responseEvent.length){
-    if(responseEvent[0].invites[0]){
+  const eventRequests = await Event.find(
+    { _id: new ObjectId(eventId) },
+    {
+      requests: {
+        $elemMatch: {
+          email: userEmail
+        }
+      }
+    }
+  );
+
+  if (eventRequests.length) {
+    if (eventRequests[0].invites[0]) {
+      //remove request
+      await Event.updateOne(
+        { _id: new ObjectId(eventId) },
+        { $pull: { requests: { email: userEmail } } }
+      );
+    }
+  }
+
+  if (responseEvent.length) {
+    if (responseEvent[0].invites[0]) {
       return "already invited";
     }
   }
 
-    return await Event.findOneAndUpdate({_id: new ObjectId(eventId)}, {$push: { invites: [newInvite] }}, {new: true});
+  return await Event.findOneAndUpdate(
+    { _id: new ObjectId(eventId) },
+    { $push: { invites: [newInvite] } },
+    { new: true }
+  );
 }
 
-export async function addRequest(event, user) {       
-    return await Event.findOneAndUpdate({_id: new ObjectId(event)}, {$push: { requests: [{email: user}] }}, {new: true});
+export async function addRequest(event, user) {
+  return await Event.findOneAndUpdate(
+    { _id: new ObjectId(event) },
+    { $push: { requests: [{ email: user }] } },
+    { new: true }
+  );
 }
 
 export function addEvent(event) {
-    const eType = event.eventType; 
-    const email = event.owner.email; 
-    const id = new ObjectId(event.owner.id);
+  const eType = event.eventType;
+  const email = event.owner.email;
+  const id = new ObjectId(event.owner.id);
 
-    const newEvent = new Event({
-      name: event.name, 
-      description: event.description, 
-      dateTime: event.dateTime, 
-      deadline: event.deadline, 
-      repeatEvery: event.repeatEvery, 
-      duration: event.duration, 
-      maxPlayers: event.maxPlayers, 
-      location: {
-        name: event.location.name, 
-        lat: event.location.lat,
-        long: event.location.long,
-      },
-      invites: [],
-      reserveInvites:[],
-      requests: [],
-      eventType: {name: eType},
-      owner: {email: email, _id: id}
-    });
+  const newEvent = new Event({
+    name: event.name,
+    description: event.description,
+    dateTime: event.dateTime,
+    deadline: event.deadline,
+    repeatEvery: event.repeatEvery,
+    duration: event.duration,
+    maxPlayers: event.maxPlayers,
+    location: {
+      name: event.location.name,
+      lat: event.location.lat,
+      long: event.location.long
+    },
+    invites: [],
+    reserveInvites: [],
+    requests: [],
+    eventType: { name: eType },
+    owner: { email: email, _id: id }
+  });
 
-    newEvent.save();
+  newEvent.save();
 
-    return newEvent;
+  return newEvent;
 }
 
-export async function updateInviteStatus(eventId, inviteId, accept) {    
+export async function updateInviteStatus(eventId, inviteId, accept) {
   let responseMessage = "invite not found";
   let responsePayload = {};
 
-  const eventToUpdate = await Event.find({ _id: new ObjectId(eventId)},
+  const eventToUpdate = await Event.find(
+    { _id: new ObjectId(eventId) },
     {
       invites: {
         $elemMatch: {
-          _id: new ObjectId(inviteId),
+          _id: new ObjectId(inviteId)
         }
       }
-  });
-  const fullEvent = await Event.find({ _id: new ObjectId(eventId)});
+    }
+  );
+  const fullEvent = await Event.find({ _id: new ObjectId(eventId) });
 
-  if(eventToUpdate.length){
-    if(eventToUpdate[0].invites.length){
-        if(eventToUpdate[0].invites[0].accepted === 'declined') {
-          responseMessage = accept ? "declined" : "already declined";
-          responsePayload = {}
-        }
+  if (eventToUpdate.length) {
+    if (eventToUpdate[0].invites.length) {
+      if (eventToUpdate[0].invites[0].accepted === "declined") {
+        responseMessage = accept ? "declined" : "already declined";
+        responsePayload = {};
+      }
 
-        if(eventToUpdate[0].invites[0].accepted === 'accepted') {
-          responseMessage = accept ? "already accepted" : "accepted";
-          responsePayload = {}
-        }
+      if (eventToUpdate[0].invites[0].accepted === "accepted") {
+        responseMessage = accept ? "already accepted" : "accepted";
+        responsePayload = {};
+      }
 
-        if(eventToUpdate[0].invites[0].accepted === 'undefined') {
-          const deadline = new Date(fullEvent[0].deadline);
-          const now = new Date();
-          if(now <= deadline){
+      if (eventToUpdate[0].invites[0].accepted === "undefined") {
+        const deadline = new Date(fullEvent[0].deadline);
+        const now = new Date();
+        if (now <= deadline) {
+          var participants = 0;
+          fullEvent[0].invites.forEach(function (i) {
+            let acc = i.accepted === "accepted" ? 1 : 0;
+            participants = participants + acc;
+          });
 
-            var participants = 0;
-            fullEvent[0].invites.forEach(function(i) {
-              let acc = (i.accepted === 'accepted') ? 1 : 0;
-              participants = participants + acc;
-            })
-
-            if(participants < fullEvent[0].maxPlayers){
+          if (participants < fullEvent[0].maxPlayers) {
             responseMessage = accept ? "accepted" : "declined";
             responsePayload = await Event.findOneAndUpdate(
               {
                 _id: new ObjectId(eventId),
-                'invites._id': inviteId,
+                "invites._id": inviteId
               },
               {
-                $set: { 'invites.$.accepted': accept ? "accepted" : "declined" },
+                $set: { "invites.$.accepted": accept ? "accepted" : "declined" }
               },
               {
                 new: true,
                 runValidators: true,
-                useFindAndModify: false,
+                useFindAndModify: false
               }
-            );   
+            );
           } else {
             responseMessage = "participants limit exceeded";
           }
         } else {
           responseMessage = "deadline exceeded";
         }
-      } 
+      }
     }
   }
-  return {message: responseMessage, payload: responsePayload};
-    
+  return { message: responseMessage, payload: responsePayload };
 }
--- src/util/constants.js
+++ src/util/constants.js
@@ -6,11 +6,20 @@ export const MESSAGE_TYPES = {
 
 export const TOKEN_EXPIRES_IN = "1h";
 
-export const EVENT_SELECTOR = {"name": 1,"description": 1,"dateTime": 1,"location": 1,"maxPlayers": 1, "owner": 1, "eventType": 1};
-
-export const appEmail = {
-  address: 'mernsummerproject@gmail.com',
-  password: 'pnxqtkyvsgnhhrus',
-  linkURL: 'http://localhost:3001/invite'
+export const EVENT_SELECTOR = {
+  name: 1,
+  description: 1,
+  dateTime: 1,
+  location: 1,
+  maxPlayers: 1,
+  owner: 1,
+  eventType: 1
 };
 
+export const appEmail = {
+  address: "mernsummerproject@gmail.com",
+  password: "pnxqtkyvsgnhhrus",
+  linkURL: "http://localhost:3001/invite"
+};
+
+export const frontend_URL = "http://localhost:3000";
